0e4055c445c99f322ea74382a21f357c21d050fe
Elibarg <calagabriel31@gmail.com>
2023-04-05 11:53:13 -0300

    a

--- tabuada.php
+++ tabuada.php
@@ -5,20 +5,33 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        .verde 
+        {
+            background-color:green;
+        }
+        .amarelo 
+        {
+            background-color:yellow;
+        }
+    </style>
 </head>
 <body>
     <?php
-    $nome = "Gabriel do Senai";
-    $resultado = (2+1)*5;
-    echo (
-    "EXEMPLO  NO PHP
-    <br>
-    o cálculo de $resultado
-    linha 2
-    Meu nome é $nome
-    "
-    );
-   
+    $valor = $_GET["valor"];
+    for($x=1;$x<=10;$x++)
+    { 
+    if ($x%2 == 0)
+    {
+     echo ("<div class='verde'>"); 
+    }
+    else
+    {
+        echo ("<div class='amarelo'>"); 
+    }
+    echo (5*$x);
+    echo ("</div> <br>");
+   }
     ?>
 </body>
 </html>
